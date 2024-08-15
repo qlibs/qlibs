1b38691cac72921ae1268b79b6454457b5c8b162
Kris Jusiak <kris@jusiak.net>
2024-08-15 12:06:51 +0100

    :wrench: release

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-LIBS=`curl -s "https://api.github.com/users/qlibs/repos" | grep "\"full_name\":" | grep -v github | grep -v "/qlibs" | cut -d '/' -f2 | cut -d \" -f1 | xargs`
+LIBS=$(curl -s "https://api.github.com/users/qlibs/repos" | grep "\"full_name\":" | grep -v github | grep -v "/qlibs" | cut -d '/' -f2 | cut -d \" -f1 | xargs)
 
 rm -rf qlibs
 mkdir qlibs
 
 for lib in $LIBS; do
-  wget -q -P qlibs https://raw.githubusercontent.com/qlibs/$lib/main/$lib
+  release=$(curl -s "https://api.github.com/repos/qlibs/$lib/releases/latest" | grep "\"tag_name\":" | cut -d \" -f4)
+
+  if [ "$release" != "" ]; then
+    wget -q -P qlibs https://raw.githubusercontent.com/qlibs/$lib/$release/$lib
+  fi
+
 done
