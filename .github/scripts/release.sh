b38d8699e5d68bceaf963cb7374ef415156c095f
Kris Jusiak <kris@jusiak.net>
2024-08-15 21:20:12 +0100

    :wrench: [release]

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -9,8 +9,8 @@ for lib in $LIBS; do
   release=$(curl -s "https://api.github.com/repos/qlibs/$lib/releases/latest" | jq -r .tag_name)
   description=$(curl -s "https://api.github.com/repos/qlibs/$lib" | jq -r .description)
 
-  if [ "$release" != "" ]; then
+  if [ "$release" != "null" ]; then
     wget -q -P qlibs https://raw.githubusercontent.com/qlibs/$lib/$release/$lib
-    echo "> $description - [$lib](https://github.com/qlibs/$lib) ([$release](https://github.com/qlibs/$lib/releases/tag/$release))"
+    echo "> - $description - [$lib](https://github.com/qlibs/$lib) ([$release](https://github.com/qlibs/$lib/releases/tag/$release))"
   fi
 done
